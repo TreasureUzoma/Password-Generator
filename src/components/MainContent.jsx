09c20a4411a1b19f9085268cf1112bff00983bff
TreasureUzoma <treasureuzoma650@gmail.com>
2024-12-05 11:51:41 +0100

    feat: chackboxes toggles effects generated password in realtime

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -6,22 +6,21 @@ import { generatePassword, evaluateStrength } from "./../main/passwordUtils"; //
 function MainContent() {
     const [checkedStates, setCheckedStates] = useState({
         upperCase: false,
-        lowerCase: true, // Always true, can't be toggled 
+        lowerCase: true, // Always true, can't be toggled
         numbers: false,
         symbols: false,
-        easyToRemember: true, 
+        easyToRemember: true
     });
     const [passwordLength, setPasswordLength] = useState(8);
     const [password, setPassword] = useState("");
-    const [strength, setStrength] = useState(2); // 0&1: weak, 2: fair, 3: good, 4: strong 
+    const [strength, setStrength] = useState(2); // 0&1: weak, 2: fair, 3: good, 4: strong
     const [strengthMessage, setStrengthMessage] = useState("FAIR");
     const [shuffling, setShuffling] = useState(false); // State to control the shuffle animation
     const [displayedPassword, setDisplayedPassword] = useState(""); // Displaying the shuffled password
     const [showCopiedPopup, setShowCopiedPopup] = useState(false); // State for the "Copied" popup
 
     // Handle range slider change
-    const handleRangeChange = (e) => {
-        
+    const handleRangeChange = e => {
         const rangeValue = e.target.value;
         setPasswordLength(parseInt(rangeValue));
 
@@ -36,30 +35,35 @@ function MainContent() {
     };
 
     // Toggle checkbox state (only for the non-disabled ones)
-    const toggleCheckbox = (type) => {
+    const toggleCheckbox = type => {
         if (type !== "lowerCase") {
-            setCheckedStates((prevState) => ({
+            setCheckedStates(prevState => ({
                 ...prevState,
-                [type]: !prevState[type],
+                [type]: !prevState[type]
             }));
         }
-    
-      handleGeneratePassword();
     };
+
+    useEffect(() => {
+        handleGeneratePassword(); // Regenerate password whenever checkedStates change
+    }, [checkedStates, passwordLength]); // Dependency array to watch changes
 
     // Function to generate a random character for the shuffle effect
     const getRandomChar = () => {
-        const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+        const chars =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
         return chars.charAt(Math.floor(Math.random() * chars.length));
     };
 
     // Simulate the shuffling effect
-    const shufflePassword = (finalPassword) => {
+    const shufflePassword = finalPassword => {
         const shuffleTimes = 6; // Number of "shuffle" steps
         let currentShuffle = 0;
         const interval = setInterval(() => {
             setDisplayedPassword(
-                Array.from({ length: passwordLength }, () => getRandomChar()).join("")
+                Array.from({ length: passwordLength }, () =>
+                    getRandomChar()
+                ).join("")
             );
             currentShuffle++;
             if (currentShuffle >= shuffleTimes) {
@@ -84,9 +88,9 @@ function MainContent() {
     };
 
     // Determine the strength bar class
-    const getStrengthClass = (barNumber) => {
+    const getStrengthClass = barNumber => {
         if (strength >= barNumber) {
-            // 0&1: weak, 2: fair, 3: good, 4: strong 
+            // 0&1: weak, 2: fair, 3: good, 4: strong
             if (strength <= 1) {
                 return "bg-red-500 border-red-500"; // Weak
             } else if (strength === 2) {
@@ -96,8 +100,7 @@ function MainContent() {
             } else if (strength >= 4) {
                 return "bg-green-500 border-green-500"; // Strong
             }
-        }
-        else {
+        } else {
             return "bg-black border-red"; // Default for unfilled bars
         }
     };
@@ -148,42 +151,54 @@ function MainContent() {
                         />
 
                         {/* Custom Checkboxes */}
-                        {["upperCase", "lowerCase", "numbers", "symbols", "easyToRemember"].map(
-                            (key) => (
-                                <div className="flex mt-3 items-center" key={key}>
-                                    <div
-                                        onClick={() => toggleCheckbox(key)}
-                                        className={`flex items-center cursor-pointer relative h-5 w-5 border border-[#34afcb] rounded shadow hover:shadow-md ${
-                                            checkedStates[key] 
-                                                ? "bg-[#34afcb]" 
-                                                : (key === "lowerCase" ? "bg-[#1a4f56]" : "")
-                                        }`}
-                                        style={{
-                                            cursor: key === "lowerCase" ? "not-allowed" : "pointer",
-                                        }}
-                                    >
-                                        {checkedStates[key] && (
-                                            <span className="absolute text-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                                                <Check />
-                                            </span>
-                                        )}
-                                    </div>
-                                    <label
-                                        className="cursor-pointer ml-3 text-sm"
-                                        onClick={() => toggleCheckbox(key)}
-                                        style={{
-                                            color: key === "lowerCase" ? "#6b7d7f" : "inherit", // Slightly darker color for disabled
-                                        }}
-                                    >
-                                        {key === "easyToRemember"
-                                            ? "Easy to Remember"
-                                            : `Include ${key
-                                                  .replace(/([A-Z])/g, " $1")
-                                                  .trim()} Letters`}
-                                    </label>
+                        {[
+                            "upperCase",
+                            "lowerCase",
+                            "numbers",
+                            "symbols",
+                            "easyToRemember"
+                        ].map(key => (
+                            <div className="flex mt-3 items-center" key={key}>
+                                <div
+                                    onClick={() => toggleCheckbox(key)}
+                                    className={`flex items-center cursor-pointer relative h-5 w-5 border border-[#34afcb] rounded shadow hover:shadow-md ${
+                                        checkedStates[key]
+                                            ? "bg-[#34afcb]"
+                                            : key === "lowerCase"
+                                            ? "bg-[#1a4f56]"
+                                            : ""
+                                    }`}
+                                    style={{
+                                        cursor:
+                                            key === "lowerCase"
+                                                ? "not-allowed"
+                                                : "pointer"
+                                    }}
+                                >
+                                    {checkedStates[key] && (
+                                        <span className="absolute text-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                                            <Check />
+                                        </span>
+                                    )}
                                 </div>
-                            )
-                        )}
+                                <label
+                                    className="cursor-pointer ml-3 text-sm"
+                                    onClick={() => toggleCheckbox(key)}
+                                    style={{
+                                        color:
+                                            key === "lowerCase"
+                                                ? "#6b7d7f"
+                                                : "inherit" // Slightly darker color for disabled
+                                    }}
+                                >
+                                    {key === "easyToRemember"
+                                        ? "Easy to Remember"
+                                        : `Include ${key
+                                              .replace(/([A-Z])/g, " $1")
+                                              .trim()} Letters`}
+                                </label>
+                            </div>
+                        ))}
 
                         <div className="flex align-center bg-[#061417] py-4 px-3 justify-between my-7">
                             <span className="text-[#1b5a68]">STRENGTH</span>
@@ -216,7 +231,7 @@ function MainContent() {
                             onClick={handleGeneratePassword}
                         >
                             <span>GENERATE </span>
-                          <i className="fa fa-arrow-right"></i>
+                            <i className="fa fa-arrow-right"></i>
                         </button>
                     </div>
                 </div>
